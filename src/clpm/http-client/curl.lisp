ba61c42785f0368a6b615e608e68aa6f6d8f7b36
Eric Timmons <etimmons@mit.edu>
2018-12-31 14:00:15 -0600

    Remove unused function

--- src/clpm/http-client/curl.lisp
+++ src/clpm/http-client/curl.lisp
@@ -49,39 +49,3 @@
                :format-control "Error from curl:~%~%~A"
                :format-arguments (list err)))
       t)))
-
-(defmethod fetch-url-to-pathname ((client curl-client) url pathname
-                                  &key headers)
-  (ensure-directories-exist pathname)
-  (flet ((write-headers-to-stream (stream)
-           (loop
-             :for (name . value) :in headers
-             :do (format stream "~A: ~A~%" name value))
-           (close stream)))
-    (multiple-value-bind (out err exit-code)
-        (uiop:run-program `("curl"
-                            ;; Add the requested headers. Pass them in on stdin
-                            ;; to prevent them from being visibile in the
-                            ;; process list (in case any of them are
-                            ;; authentication headers...)
-                            "-H" "@-"
-                            ;; Follow any redirects
-                            "-L"
-                            ;; Don't read any user config files
-                            "-q"
-                            ;; Bail out on non-200s
-                            "--fail"
-                            ;; Be quiet, but still show the error (if one occurs)
-                            "--silent"
-                            "--show-error"
-                            ,(uri-to-string url))
-                          :input #'write-headers-to-stream
-                          :output pathname
-                          :error-output '(:string :stripped t)
-                          :ignore-error-status t)
-      (declare (ignore out))
-      (unless (zerop exit-code)
-        (error 'simple-fetch-error
-               :format-control "Error from curl:~%~%~A"
-               :format-arguments (list err)))
-      t)))
--- src/clpm/http-client/defs.lisp
+++ src/clpm/http-client/defs.lisp
@@ -1,7 +1,6 @@
 (uiop:define-package #:clpm/http-client/defs
     (:use #:cl)
   (:export #:fetch-error
-           #:fetch-url-to-pathname
            #:fetch-url-to-stream
            #:simple-fetch-error))
 
@@ -12,8 +11,5 @@
 
 (define-condition simple-fetch-error (fetch-error simple-error)
   ())
-
-(defgeneric fetch-url-to-pathname (client url pathname
-                                   &key headers))
 
 (defgeneric fetch-url-to-stream (client url stream &key headers))
