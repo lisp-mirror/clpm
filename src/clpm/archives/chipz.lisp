27cd82b7faa3784925b9bc59644af545d8737a60
Eric Timmons <etimmons@mit.edu>
2019-01-01 17:01:26 -0600

    Add a method that is missing from chipz

--- src/clpm/archives/chipz.lisp
+++ src/clpm/archives/chipz.lisp
@@ -5,6 +5,10 @@
 
 (in-package #:clpm/archives/chipz)
 
+;; This is missing from chipz...
+(defmethod stream-element-type ((stream chipz::decompressing-stream))
+  '(unsigned-byte 8))
+
 (defmethod unarchive ((archive-type gzipped-archive) archive-stream destination-pathname)
   (call-next-method archive-type (chipz:make-decompressing-stream 'chipz:gzip archive-stream)
                     destination-pathname))
