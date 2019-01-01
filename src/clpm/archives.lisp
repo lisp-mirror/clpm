62197dae58c149500f4c74f39e064cbdd8dcf11e
Eric Timmons <etimmons@mit.edu>
2019-01-01 13:18:11 -0600

    Don't load archive asd system

--- src/clpm/archives.lisp
+++ src/clpm/archives.lisp
@@ -1,7 +1,6 @@
 (uiop:define-package #:clpm/archives
     (:use #:cl
           #:clpm/archives/defs
-          #:clpm/archives/archive
           #:clpm/archives/chipz
           #:clpm/archives/tar)
   (:reexport #:clpm/archives/defs))
