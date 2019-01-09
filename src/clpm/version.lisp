86958d175f63f50f12645a2c24546cec5fe74f16
Eric Timmons <etimmons@mit.edu>
2019-01-09 15:57:28 -0500

    Version 0.0.6

--- src/clpm/version.lisp
+++ src/clpm/version.lisp
@@ -14,7 +14,7 @@
 
 ;; NOTE: If this form changes position in this file you *must* update clpm.asd as
 ;; well.
-(defparameter *version* "0.0.5")
+(defparameter *version* "0.0.6")
 
 (defparameter *git-version* "")
 
