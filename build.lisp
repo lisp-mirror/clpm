b0c1285f3590ee7c9bf6666efa89966ded648898
Eric Timmons <etimmons@mit.edu>
2019-01-10 15:57:42 -0500

    Used wrong pathname in previous commit

--- build.lisp
+++ build.lisp
@@ -59,7 +59,7 @@ process."
                :do
                   (print arg out)))))
     (apply #'run-program
-           (list (uiop:native-namestring (merge-pathnames "scripts/clpm-live" *build-root-pathname*)))
+           (list (uiop:native-namestring (merge-pathnames "scripts/clpm-live" *root-pathname*)))
            :output :interactive
            :error-output :interactive
            :input #'vomit
