d529b3b03bc17589268c787e6bab871608e29b87
Eric Timmons <etimmons@mit.edu>
2019-01-28 20:32:22 -0500

    Start using lake to manage useful build tasks

---
+++ docker/customize-target-features.lisp
@@ -0,0 +1,11 @@
+;;;; File to customize SBCL's features while building it in Docker
+
+(lambda (list)
+  (flet ((enable (x)
+           (pushnew x list))
+         (disable (x)
+           (setf list (remove x list))))
+    (enable :sb-core-compression)
+    (enable :sb-dynamic-core)
+    (enable :sb-linkable-runtime))
+  list)
