1a285bda66d5cd1e2844ae451fd4d4ae86c01306
Eric Timmons <etimmons@mit.edu>
2020-02-20 17:16:29 -0500

    Fix install.sh for nested directories

--- install.sh
+++ install.sh
@@ -2,16 +2,23 @@
 
 INSTALL_ROOT=${INSTALL_ROOT:-/usr/local}
 
-install -d "$INSTALL_ROOT/bin"
-install -d "$INSTALL_ROOT/lib/clpm"
-install -d "$INSTALL_ROOT/man/man1"
+install_directory() {
+  install -d "$INSTALL_ROOT/$1"
+  find "build/$1" -maxdepth 1 -type f -exec install -t "$INSTALL_ROOT/$1/" {} \;
+  # I don't think this works with hidden directories, but we have none of
+  # those...
+  for f in "build/$1"/*; do
+    if [ -d "$f" ]; then
+      install_directory "$1/$(basename "$f")"
+    fi
+  done
 
-install -T build/bin/clpm "$INSTALL_ROOT/bin/clpm"
+}
 
-for f in build/lib/clpm/*; do
-    install -T "$f" "$INSTALL_ROOT/lib/clpm/$(basename "$f")"
-done
+install_directory bin
+install_directory lib/clpm
+install_directory man/man1
 
-for f in build/man/man1/*; do
-    install -T "$f" "$INSTALL_ROOT/man/man1/$(basename "$f")"
-done
+# Local Variables:
+# sh-basic-offset: 2
+# End:
