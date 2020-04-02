6aed82119ff5f4ae3c09f5f3307d45536d5bd84e
Eric Timmons <etimmons@mit.edu>
2020-04-02 17:17:04 -0400

    CLPM v0.2.0

--- src/clpm/version.lisp
+++ src/clpm/version.lisp
@@ -19,7 +19,7 @@
   "ASDF is overly picky about its version numbers. This should be the primary
   version number (no prerelease info) as ~*base-version*~.")
 
-(defparameter *base-version* "0.2.0-alpha.4"
+(defparameter *base-version* "0.2.0"
   "The base version number of CLPM.")
 
 (defparameter *full-version* nil
