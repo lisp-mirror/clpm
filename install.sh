1f71b65aa70f78b0fac4f81ef6690163f4b58c4d
Eric Timmons <etimmons@mit.edu>
2020-04-01 21:13:01 -0400

    Fix install.sh for Darwin

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ INSTALL_ROOT=${INSTALL_ROOT:-/usr/local}
 
 install_directory() {
   install -d "$INSTALL_ROOT/$1"
-  find "build/$1" -maxdepth 1 -type f -exec install -t "$INSTALL_ROOT/$1/" {} \;
+  find "build/$1" -maxdepth 1 -type f -exec install {} "$INSTALL_ROOT/$1/" \;
   # I don't think this works with hidden directories, but we have none of
   # those...
   for f in "build/$1"/*; do
