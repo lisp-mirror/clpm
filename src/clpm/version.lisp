e93e00f8ac18b1636c0af503143323da91f4f6f6
Eric Timmons <etimmons@mit.edu>
2020-04-28 17:58:17 -0400

    Begin work on v0.3.0

--- src/clpm/version.lisp
+++ src/clpm/version.lisp
@@ -15,11 +15,11 @@
 
 ;; NOTE: If this form changes position in this file you *must* update clpm.asd as
 ;; well.
-(defparameter *version-for-asdf* "0.2.1"
+(defparameter *version-for-asdf* "0.3.0"
   "ASDF is overly picky about its version numbers. This should be the primary
   version number (no prerelease info) as ~*base-version*~.")
 
-(defparameter *base-version* "0.2.1"
+(defparameter *base-version* "0.3.0"
   "The base version number of CLPM.")
 
 (defparameter *full-version* nil
