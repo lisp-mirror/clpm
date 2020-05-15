d553e31fcecc81413642697672ee1a59e5828568
Eric Timmons <etimmons@mit.edu>
2020-05-15 13:06:08 -0400

    cleanup: move licenses reader into CLI system

--- cli/license-info.lisp
+++ cli/license-info.lisp
@@ -6,12 +6,45 @@
 (uiop:define-package #:clpm-cli/license-info
     (:use #:cl
           #:clpm-cli/common-args
-          #:clpm-cli/interface-defs
-          #:clpm-licenses)
+          #:clpm-cli/interface-defs)
   (:import-from #:uiop
                 #:*stdout*))
 
 (in-package #:clpm-cli/license-info)
+
+(defparameter *license-root-dir*
+  (asdf:system-relative-pathname :clpm-cli "licenses/")
+  "Points to the root directory of licenses in the CLPM source folder. Is
+cleared when UIOP's clear configuration hook is run (such as on image dump).")
+
+(defun clear-root-dir ()
+  (setf *license-root-dir* nil))
+
+(uiop:register-clear-configuration-hook 'clear-root-dir)
+
+(defparameter *licenses* (make-hash-table :test 'equalp)
+  "A hash table that maps project names to license strings.")
+
+(defparameter *notices* (make-hash-table :test 'equalp)
+  "A hash table that maps project names to NOTICE strings.")
+
+(defparameter *clpm-license* nil)
+
+(defun load-licenses ()
+  "Load all licenses from *license-root-dir*."
+  (let ((files (uiop:directory* (uiop:wilden *license-root-dir*))))
+    (dolist (f files)
+      (let ((name (pathname-name f))
+            (type (pathname-type f))
+            (contents (uiop:read-file-string f)))
+        (unless (equal name "README")
+          (if (equal type "NOTICE")
+              (setf (gethash name *notices*) contents)
+              (setf (gethash name *licenses*) contents)))))
+    (setf *clpm-license* (uiop:read-file-string (asdf:system-relative-pathname :clpm "LICENSE")))))
+
+(eval-when (:load-toplevel)
+  (load-licenses))
 
 (defparameter *license-info-ui*
   (adopt:make-interface
