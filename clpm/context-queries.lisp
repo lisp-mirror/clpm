31922c0113e81bf39454cab46c84df199ad963f6
Eric Timmons <etimmons@mit.edu>
2020-05-19 14:14:42 -0400

    Add some functions to context queries

--- clpm/context-queries.lisp
+++ clpm/context-queries.lisp
@@ -6,11 +6,14 @@
 (uiop:define-package #:clpm/context-queries
     (:use #:cl
           #:clpm/context
-          #:clpm/session)
+          #:clpm/session
+          #:clpm/source)
   (:export #:asd-pathnames
            #:find-system-asd-pathname
+           #:installed-system-names
            #:output-translations
-           #:source-registry))
+           #:source-registry
+           #:visible-primary-system-names))
 
 (in-package #:clpm/context-queries)
 
@@ -21,6 +24,9 @@
 (defun find-system-asd-pathname (system-name &key context)
   (with-clpm-session ()
     (context-find-system-asd-pathname (get-context context) system-name)))
+
+(defun installed-system-names (&key context)
+  (mapcar 'system-name (context-installed-systems (get-context context))))
 
 (defun output-translations (&key context)
   (with-clpm-session ()
@@ -35,3 +41,6 @@
      :with-client with-client-p
      :ignore-inherited ignore-inherited-source-registry
      :splice-inherited splice-inherited)))
+
+(defun visible-primary-system-names (&key context)
+  (context-visible-primary-system-names (get-context context)))
