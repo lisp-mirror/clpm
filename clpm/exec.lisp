d6aaab119679d8fb6e0f9073cd3a56076e0c9419
Eric Timmons <etimmons@mit.edu>
2020-05-19 11:11:17 -0400

    Typo

--- clpm/exec.lisp
+++ clpm/exec.lisp
@@ -43,7 +43,7 @@ If WITH-CLIENT-P is non-NIL, the clpm-client system is available."
         (execvpe command args
                  `(("CL_SOURCE_REGISTRY" . ,(format nil "~S" source-registry))
                    ,@(when output-translations
-                       `(("ASDF_OUTPUT_TRANSLATIONS" . (format nil "~S" output-translations))))
+                       `(("ASDF_OUTPUT_TRANSLATIONS" . ,(format nil "~S" output-translations))))
                    ("CLPM_EXEC_CONTEXT" . ,context-name)
                    ("CLPM_EXEC_INSTALLED_SYSTEMS" . ,(format nil "~{~A~^ ~}" installed-system-names))
                    ("CLPM_EXEC_VISIBLE_PRIMARY_SYSTEMS" . ,(format nil "~{~A~^ ~}" visible-primary-system-names))
