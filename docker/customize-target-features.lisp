34dcceb005e736d63c1ac411a984b87459bee79a
Eric Timmons <etimmons@mit.edu>
2020-05-21 20:39:53 -0400

    Do not build with core compression
    Windows already doesn't do it and I don't want to require libz.

--- docker/customize-target-features.lisp
+++
@@ -1,9 +0,0 @@
-;;;; File to customize SBCL's features while building it in Docker
-
-(lambda (list)
-  (flet ((enable (x)
-           (pushnew x list))
-         (disable (x)
-           (setf list (remove x list))))
-    (enable :sb-core-compression))
-  list)
