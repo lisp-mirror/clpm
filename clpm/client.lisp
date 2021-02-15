bd139fb30dd0a1539c959abd58e69e2c7098f71a
Eric Timmons <etimmons@mit.edu>
2021-02-15 13:55:53 -0500

    Bundle client as a tarball in the program

--- clpm/client.lisp
+++ clpm/client.lisp
@@ -4,15 +4,31 @@
 ;;;; LICENSE for license information.
 
 (uiop:define-package #:clpm/client
-    (:use #:cl)
+    (:use #:cl
+          #:clpm/archives
+          #:clpm/data
+          #:clpm/version)
+  (:import-from #:flexi-streams
+                #:make-in-memory-input-stream)
   (:export #:*clpm-client-asd-pathname*
+           #:*clpm-client-tarball-contents*
            #:client-asd-pathname))
 
 (in-package #:clpm/client)
 
-(defvar *clpm-client-asd-pathname*
-  (asdf:system-relative-pathname :clpm "client/clpm-client.asd"))
+(defvar *clpm-client-tarball-contents* nil)
+
+(defun unpack-client ()
+  (let ((dir-pathname (clpm-data-pathname `("client"
+                                            ,(concatenate 'string "clpm-client-" (clpm-version)))
+                                          :ensure-directory t)))
+    (unless (probe-file dir-pathname)
+      (unarchive 'gzipped-tar-archive (make-in-memory-input-stream *clpm-client-tarball-contents*)
+                 dir-pathname :strip-components 1))
+    (merge-pathnames "clpm-client.asd" dir-pathname)))
 
 (defun client-asd-pathname ()
   "Return the pathname to the client .asd file."
-  *clpm-client-asd-pathname*)
+  (if *clpm-client-tarball-contents*
+      (unpack-client)
+      (asdf:system-source-file :clpm-client)))
