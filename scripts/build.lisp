da43be0b98eddb98aa019665fe63cd2be4fd8c46
Eric Timmons <etimmons@mit.edu>
2021-02-18 12:14:40 -0500

    Try to speed up builds on CI

--- scripts/build.lisp
+++ scripts/build.lisp
@@ -16,4 +16,15 @@
         *root-pathname*
         *build-root-pathname*)
 
+;; TODO: This is a bit hacky, but speeds up the build significantly when
+;; starting from scratch (like in CI). The root problem is that
+;; asdf-release-ops will occasionally build the same code twice: once in the
+;; child process and once in the parent. This is because we use
+;; asdf:monolithic-lib-op in the parent. However, moving that op to the child
+;; didn't quite work as it would error out due to package variance in
+;; dexador...
+
+(asdf:load-system :clpm)
+(asdf:load-system :clpm-cli)
+
 (asdf:make :clpm)
