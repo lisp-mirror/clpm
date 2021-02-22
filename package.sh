34a743c25b32171b10c5ca888ed4b19104d6682e
Eric Timmons <etimmons@mit.edu>
2021-02-21 21:19:41 -0500

    Update documentation and cleanup some files

--- package.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-BASE="${1:-.}"
-tar -cvf "$BASE/build/clpm.tar" "$BASE/build/bin/" "$BASE/build/lib/" "$BASE/build/man/" "$BASE/LICENSE" "$BASE/README.org" "$BASE/install.sh"
-gzip "$BASE/build/clpm.tar"
